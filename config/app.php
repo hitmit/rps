fb8daaa633027ef8e13ce6540fa54626657cd8f7
Hitesh <hm@loclio.com>
2016-04-21 13:29:48 +0530

    collective html added to system

--- config/app.php
+++ config/app.php
@@ -148,6 +148,9 @@ return [
         // AdminLTE template provider
         Acacha\AdminLTETemplateLaravel\app\Providers\AdminLTETemplateServiceProvider::class,
 
+        // HTML and FORM class.
+        Collective\Html\HtmlServiceProvider::class,
+
     ],
 
     /*
@@ -195,6 +198,10 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+
+        // Form and HTML alias.
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
